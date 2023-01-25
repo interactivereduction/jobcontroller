ed794978e605da9a33750381d9a887cc330c07db
Samuel Jones <samuel.jones@stfc.ac.uk>
2023-01-25 09:51:16 +0000

    Add some pylint fixes

--- test/test_jobcontroller.py
+++ test/test_jobcontroller.py
@@ -1,3 +1,6 @@
+"""
+The tests for the jobcontroller class
+"""
 import unittest
 from unittest import mock
 
@@ -5,19 +8,22 @@ from jobcontroller.jobcontroller import JobController
 
 
 class JobControllerTest(unittest.TestCase):
+    """
+    The unittest test class
+    """
     @mock.patch("jobcontroller.jobcontroller.K8sAPI")
     @mock.patch("jobcontroller.jobcontroller.TopicConsumer")
     def setUp(self, _, __):
-        self.jc = JobController()
+        self.joc = JobController()
 
     def test_on_message_calls_spawn_pod_with_message(self):
         message = mock.MagicMock()
 
-        self.jc.on_message(message)
+        self.joc.on_message(message)
 
-        self.jc.k8s.spawn_pod.assert_called_once_with(message)
+        self.joc.k8s.spawn_pod.assert_called_once_with(message)
 
     def test_run_class_start_consuming(self):
-        self.jc.run()
+        self.joc.run()
 
-        self.jc.consumer.start_consuming.assert_called_once_with()
+        self.joc.consumer.start_consuming.assert_called_once_with()
