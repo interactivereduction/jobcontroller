4d2ca054b7ba60edd658c0ce4bf63818125322f5
Samuel Jones <samuel.jones@stfc.ac.uk>
2023-06-14 10:42:38 +0100

    Fix issues with the job controller tests

--- test/test_job_controller.py
+++ test/test_job_controller.py
@@ -11,24 +11,40 @@ from job_controller.main import JobController
 class JobControllerTest(unittest.TestCase):
     @mock.patch("job_controller.main.DBUpdater")
     @mock.patch("job_controller.main.JobCreator")
-    @mock.patch("job_controller.main.TopicConsumer")
+    @mock.patch("job_controller.main.StationConsumer")
     def setUp(self, _, __, ___):
         os.environ["RUNNER_SHA"] = "literally_anything"
         self.joc = JobController()
 
     @mock.patch("job_controller.main.JobCreator")
-    @mock.patch("job_controller.main.TopicConsumer")
-    def test_job_controller_gets_kafka_ip_from_env(self, _, __):
-        self.assertEqual(self.joc.kafka_ip, "")
+    @mock.patch("job_controller.main.StationConsumer")
+    def test_job_controller_gets_various_vars_from_env(self, _, __):
+        self.assertEqual(self.joc.ir_api_host, "ir-api-service.ir.svc.cluster.local:80")
+        self.assertEqual(self.joc.broker_ip, "")
+        self.assertEqual(self.joc.reduce_user_id, "")
+        self.assertEqual(self.joc.consumer_username, "")
+        self.assertEqual(self.joc.consumer_password, "")
 
-        os.environ["KAFKA_IP"] = "random_ip_address_from_kafka"
+        os.environ["IR_API"] = "fancy_ir_api_ip"
+        os.environ["BROKER_IP"] = "random_ip_address_from_broke"
+        os.environ["REDUCE_USER_ID"] = "reduceuser"
+        os.environ["CONSUMER_USERNAME"] = "usernameforconsuming"
+        os.environ["CONSUMER_PASSWORD"] = "passwordforconsuming"
 
-        self.assertEqual(JobController().kafka_ip, "random_ip_address_from_kafka")
+        self.assertEqual(JobController().ir_api_host, "fancy_ir_api_ip")
+        self.assertEqual(JobController().broker_ip, "random_ip_address_from_broke")
+        self.assertEqual(JobController().reduce_user_id, "reduceuser")
+        self.assertEqual(JobController().consumer_username, "usernameforconsuming")
+        self.assertEqual(JobController().consumer_password, "passwordforconsuming")
 
-        os.environ.pop("KAFKA_IP")
+        os.environ.pop("IR_API")
+        os.environ.pop("BROKER_IP")
+        os.environ.pop("REDUCE_USER_ID")
+        os.environ.pop("CONSUMER_USERNAME")
+        os.environ.pop("CONSUMER_PASSWORD")
 
     @mock.patch("job_controller.main.JobCreator")
-    @mock.patch("job_controller.main.TopicConsumer")
+    @mock.patch("job_controller.main.StationConsumer")
     def test_job_controller_gets_runner_sha_from_env(self, _, job_creator):
         runner_sha = mock.MagicMock()
         os.environ["RUNNER_SHA"] = str(runner_sha)
@@ -38,7 +54,7 @@ class JobControllerTest(unittest.TestCase):
         job_creator.assert_called_once_with(runner_sha=str(runner_sha))
 
     @mock.patch("job_controller.main.JobCreator")
-    @mock.patch("job_controller.main.TopicConsumer")
+    @mock.patch("job_controller.main.StationConsumer")
     def test_job_controller_raises_if_runner_sha_not_set(self, _, __):
         os.environ.pop("RUNNER_SHA")
 
@@ -134,7 +150,8 @@ class JobControllerTest(unittest.TestCase):
         threading.Thread.assert_called_once_with(target=job_watcher.return_value.watch)
         threading.Thread.return_value.start.assert_called_once_with()
 
-    def test_run_class_starts_consuming(self):
-        self.joc.run()
+    async def test_run_class_starts_consuming(self):
+        await self.joc._init()
+        await self.joc.run()
 
         self.joc.consumer.start_consuming.assert_called_once_with()
